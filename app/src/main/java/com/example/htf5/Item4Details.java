07a6f62a46f0c9ef7fcd98c3d6de80c124213b85
Roger Ye <z.rogerye@gmail.com>
2023-03-04 05:40:47 -0600

    Added OnClickListener For Feed Items

---
+++ app/src/main/java/com/example/htf5/Item4Details.java
@@ -0,0 +1,13 @@
+package com.example.htf5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Item4Details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item4_details);
+    }
+}
--- app/src/main/java/com/example/htf5/MainActivity.java
+++ app/src/main/java/com/example/htf5/MainActivity.java
@@ -1,13 +1,56 @@
 package com.example.htf5;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.widget.GridLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    GridLayout mainGrid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainGrid = (GridLayout) findViewById(R.id.mainGrid);
+        setSingleEvent(mainGrid);
+
+
+    }
+
+    private void setSingleEvent(GridLayout mainGrid){
+        for(int i = 0; i < mainGrid.getChildCount(); i++){
+            CardView cardView = (CardView) mainGrid.getChildAt(i);
+            final int finalI = i;
+            cardView.setOnClickListener((view) ->{
+                if(finalI == 0){
+                    Intent intent = new Intent(MainActivity.this, Item1Details.class);
+                    startActivity(intent);
+                }
+                if(finalI == 1){
+                    Intent intent = new Intent(MainActivity.this, Item2Details.class);
+                    startActivity(intent);
+                }
+                if(finalI == 2){
+                    Intent intent = new Intent(MainActivity.this, Item3Details.class);
+                    startActivity(intent);
+                }
+                if(finalI == 3){
+                    Intent intent = new Intent(MainActivity.this, Item4Details.class);
+                    startActivity(intent);
+                }
+                if(finalI == 4){
+                    Intent intent = new Intent(MainActivity.this, Item5Details.class);
+                    startActivity(intent);
+                }
+                if(finalI > 4){
+                    Toast.makeText(MainActivity.this, "Please set activity", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 }
